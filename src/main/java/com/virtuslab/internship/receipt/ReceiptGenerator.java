1194a7aa74773e1c1cd7a69dbe35ca6178daa6c8
Radosław Godlewski <rgodlewski@stud.elka.pw.edu.pl>
2022-03-28 23:23:36 +0200

    Fixed source code, so the test pass

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -4,11 +4,18 @@ import com.virtuslab.internship.basket.Basket;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
+
         List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        basket.getProducts()
+        .stream()
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(x -> 1)))
+                .forEach((product, quantity) -> receiptEntries.add(new ReceiptEntry(product, quantity)));
         return new Receipt(receiptEntries);
     }
 }
